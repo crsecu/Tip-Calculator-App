7b0d7b0d8b89ae2eb08dae8cc6a10519f15034d6
Cristina Secu <secu.cristina@yahoo.com>
2023-08-06 18:31:59 -0400

    save current version before attempting to wire Number of People input field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import React, {useState} from 'react';
 import './App.css';
 import BillDetails  from './components/billDetails';
-
+import Button from './components/button';
 
 function App() {
+  const [bill, setBill] = React.useState(
+      {
+        billAmount: "",
+        numberOfPeople: ""
+       }
+  );
+
+  console.log(bill);
+
+  function billAmount(event) {
+    const {name, value} = event.target;
+    setBill(prevState => {
+      return {
+        ...prevState,
+        [name]: value
+      }
+    })
+      
+    
+  }
+
+  //function that takes in tip percentage and calculates tip
+  function handleClick(event) {
+    const value =  event.target.value;
+    const tipPercentage = parseInt(value);
+    const billAmount = parseInt(bill.billAmount);
+    const tipAmount = (tipPercentage / 100) * billAmount;
+    console.log(tipAmount);
+  }
+
   return (
     <>
-      <BillDetails />
+      <BillDetails state ={billAmount} name = "billAmount"/>
+      <Button tip = "5" handleClick = {handleClick}/>
+      <Button tip = "10" handleClick = {handleClick}/>
+      <Button tip = "15" handleClick = {handleClick}/>
+      <Button tip = "20" handleClick = {handleClick}/>
+      <Button tip = "25" handleClick = {handleClick}/>
+      <Button tip = "50" handleClick = {handleClick}/>
+       
+      <BillDetails state ={billAmount} name = "numberOfPeople" />
     </>
     
   )
--- src/components/billDetails.js
+++ src/components/billDetails.js
@@ -1,35 +1,16 @@
 import React, {useState} from 'react';
-import Button from './button';
-
-export default function BillDetails() {
-
-const [bill, setBill] = React.useState('');
 
 
-function billAmount(event) {
-    setBill(event.target.value);
+export default function BillDetails(props) {
 
-}
-
-//function that takes in tip percentage and calculates tip
-    function handleClick(event) {
-       
-        const tipPercentage = parseInt(event.target.value);
-        
-        const billAmount = parseInt(bill);
-      
-        const tipAmount = (tipPercentage / 100) *  billAmount;
-        
-    }
 
      return (
       <div>
         <form>
-          <input type="number" name="amount" onChange={billAmount}></input>
+          <input type="number" name={props.name} onChange={props.state}></input>
         </form>
 
-        <Button tip = "10" handleClick = {handleClick}/>
-        <Button tip = "20" handleClick = {handleClick}/>
+        
       </div>
     );
 }
