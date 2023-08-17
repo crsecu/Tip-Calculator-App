ab81c608f035c126a28fcf8f1aa82b213e475ec9
Cristina Secu <secu.cristina@yahoo.com>
2023-08-17 16:40:33 -0400

    continue styling - mobile almost done

--- src/components/results.js
+++ src/components/results.js
@@ -5,16 +5,20 @@ export default function Results(props) {
 
 
     return (
-        <div>
-            <div>
-                <span>Tip Amount</span>
-                <span>/person </span>
-                {props.perPersonTip === 0  ? <span> $0.00</span> : <span> {props.perPersonTip}</span>}
+        <div className="tip-calculator__tip-summary-container">
+            <div className="tip-calculator__tip-summary">
+                <div className="tip-calculator__tip-info">
+                    <span className="tip-calculator__tip-label" >Tip Amount</span>
+                    <span>/person </span>
+                </div>
+                {props.perPersonTip === 0  ? <span className = "tip-calculator__amount-due"> $0.00</span> : <span className = "tip-calculator__amount-due"> ${props.perPersonTip}</span>}
             </div>
-            <div>
-                <span>Total </span>
-                <span>/person </span>
-                {props.totalBillPerPerson === 0 ? <span> $0.00</span> : <span> {props.totalBillPerPerson}</span>}
+            <div className="tip-calculator__tip-summary">
+                <div className="tip-calculator__tip-info">
+                    <span className="tip-calculator__tip-label">Total </span>
+                    <span>/person </span>
+                </div>
+                {props.totalBillPerPerson === 0 ? <span className = "tip-calculator__amount-due"> $0.00</span> : <span className = "tip-calculator__amount-due"> ${props.totalBillPerPerson}</span>}
             </div>    
         </div>
     )
